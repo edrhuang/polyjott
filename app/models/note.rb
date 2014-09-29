ed6b37d31ea9bf077cc1fd76f0a55b1ea6c8e080
Edward Huang <edrhuang@gmail.com>
2014-09-29 09:10:13 -0700

    updated commentsa and added styling

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,10 @@
 class Note < ActiveRecord::Base
 	has_many :votes, dependent: :destroy 
 	
-	has_many :comments
+	has_many :comments, dependent: :destroy 
 	validates :title, presence: true,
-		length: {minimum: 5 }
-		
+		length: { minimum: 5 }
+
 	validates :title, 
 		presence: true,
 		length: { minimum: 5 }
